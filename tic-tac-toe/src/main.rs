f8e833f3af1d11499f3ef2ed6142748668405deb
mirandap <margo.rozhkina@bk.ru>
2024-10-10 00:57:14 +0300

    -
    функция по созданию поля

--- tic-tac-toe/src/main.rs
+++ tic-tac-toe/src/main.rs
@@ -1,28 +1,25 @@
-const W: usize = 30;
+const W: usize = 50;
+const H: usize = 20;
+const KOEF_1: f64 = 0.33;
+const KOEF_2: f64 = 0.66;
 
 fn main() {
     game();
 }
 
 fn game() {
-    let mut place: [[char;W];W] = [[' '; W]; W];
+    let mut place: [[char;W];H] = [[' '; W]; H];
 
-    for i in 0..W {
-        for j in 0..W {
-            if i==0 || j==0 || i==W-1 || j==W-1 || j==(W as f64/0.3)as usize || j==(W as f64/0.6)as usize || i==(W as f64/0.3)as usize || i==(W as f64/0.6)as usize {
-                place[i][j]='0';
-            }
-        }
-    }
+    create_place(&mut place);
 
     print_legent(1);
     print_place(place);
 }
 
-fn print_place(place: [[char;W];W]) {
-    for i in 0..W {
+fn print_place(place: [[char;W];H]) {
+    for i in 0..H {
         for j in 0..W {
-            print!("{} ",place[i][j]);
+            print!("{}",place[i][j]);
         }
         println!("");
     }
@@ -54,3 +51,49 @@ fn print_legent(player: u8) {
     println!("");
 }
 
+fn create_place(place: &mut[[char;W];H]) {
+    let set_w1 = (W as f64 * KOEF_1)as usize;
+    let set_w2 = (W as f64 * KOEF_2)as usize;
+    let set_h1 = (H as f64 * KOEF_1)as usize;
+    let set_h2 = (H as f64 * KOEF_2)as usize; 
+
+    for i in 0..H {
+        for j in 0..W {
+            if i==0 && j==0 {
+                place[i][j]='┌';
+            }
+            else if i==0 && j==W-1 {
+                place[i][j]='┐';
+            }
+            else if i==H-1 && j==0 {
+                place[i][j]='└';
+            }
+            else if i==H-1 && j==W-1 {
+                place[i][j]='┘';
+            }
+            else if (i==0 && j==set_w1) || (i==0 && j==set_w2) {
+                place[i][j]='┬';
+            }
+            else if (i==H-1 && j==set_w1) || (i==H-1 && j==set_w2) {
+                place[i][j]='┴';
+            }
+            else if (j==0 && i==set_h1) || (j==0 && i==set_h2) {
+                place[i][j]='├';
+            }
+            else if (j==W-1 && i==set_h1) || (j==W-1 && i==set_h2) {
+                place[i][j]='┤';
+            }
+            else if (i==set_h1 && j==set_w1) || (i==set_h1 && j==set_w2)
+                    || (i==set_h2 && j==set_w1) || (i==set_h2 && j==set_w2) {
+                place[i][j]='┼';        
+            }
+            else if j==0  || j==W-1 || j==set_w1 || j==set_w2 {
+                place[i][j]='│';
+            }
+            else if i==0 || i==H-1 || i==set_h1 || i==set_h2 {
+                place[i][j]='─';
+            }
+            
+        }
+    }
+}
